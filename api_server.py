48d2b252a53c349196757731463e1dc9c32b7f03
zenxixxb <akkayntbobrow@gmail.com>
2026-09-22 18:30:37 +0300

    api_server.py

--- api_server.py
+++ api_server.py
@@ -1,8 +1,10 @@
 import json
 import os
 from flask import Flask, request, jsonify, send_from_directory
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 BALANCE_FILE = "balance.json"
 DATA_FILE = "products.json"
